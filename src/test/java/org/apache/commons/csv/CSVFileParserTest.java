14ea734cd348edaa1efaa154a8121813161b685f
vdvlkmr713 <vnthkmr42@gmail.com>
2015-10-26 18:04:58 +0530

    Update CSVFileParserTest.java

--- src/test/java/org/apache/commons/csv/CSVFileParserTest.java
+++ src/test/java/org/apache/commons/csv/CSVFileParserTest.java
@@ -13,7 +13,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */
+ *
 
 package org.apache.commons.csv;
 
@@ -44,7 +44,7 @@ import org.junit.runners.Parameterized.Parameters;
  * Parse tests using test files
  *
  * @version $Id$
- */
+ 
 @RunWith(Parameterized.class)
 public class CSVFileParserTest {
 
@@ -175,3 +175,4 @@ public class CSVFileParserTest {
         parser.close();
     }
 }
+*/
